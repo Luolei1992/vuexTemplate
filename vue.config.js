4de81932aa80c4986f4432fa4dc0001a975c5832
luolei <16657115156@163.com>
2020-03-17 10:22:47 +0800

    css的build问题

--- vue.config.js
+++ vue.config.js
@@ -56,20 +56,6 @@ module.exports = {
   // 是否为生产环境构建生成 source map？
   productionSourceMap: false,
 
-  // CSS 相关选项
-  css: {
-      // 将组件内的 CSS 提取到一个单独的 CSS 文件 (只用在生产环境中)
-      extract: true,
-      // 是否开启 CSS source map？
-      sourceMap: false,
-      // 为预处理器的 loader 传递自定义选项。比如传递给
-      // sass-loader 时，使用 `{ sass: { ... } }`。
-      loaderOptions: {},
-      // 为所有的 CSS 及其预处理文件开启 CSS Modules。
-      // 这个选项不会影响 `*.vue` 文件。
-      requireModuleExtension: false
-  },
-
 
   // PWA 插件的选项。
   // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli-plugin-pwa/README.md
